c72f183e26621dc419136256d48617d6a278c43b
kakas <shrimptrain@gmail.com>
2016-04-10 12:47:11 +0800

    購物車加入數量設計

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -1,11 +1,24 @@
 class CartItemsController < ApplicationController
 
+  def update
+    @item = current_cart.cart_items.find_by_product_id(params[:id])
+    @item.update(item_params)
+
+    redirect_to carts_path
+  end
+
   def destroy
     @item = current_cart.cart_items.find_by_product_id(params[:id])
     flash[:warning] = "成功將#{@item.product.title}從購物車刪除"
     @item.destroy
 
     redirect_to :back
+  end
+
+  private
+
+  def item_params
+    params.require(:cart_item).permit(:quantity)
   end
 
 end
