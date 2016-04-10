8eb370b4d5f47d484e275df91f9b7f8549164f24
kakas <shrimptrain@gmail.com>
2016-04-10 19:56:22 +0800

    管理者可以看到系統內所有訂單

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,2 +1,12 @@
 class Admin::OrdersController < ApplicationController
+
+  layout "admin"
+
+  before_action :authenticate_user!
+  before_action :admin_required
+
+  def index
+    @orders = Order.includes(:user).order("id DESC")
+  end
+
 end
